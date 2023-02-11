d606b07a55098ae0bab0886b574c03d17068dbaa
alannuarii <alannuariii@gmail.com>
2023-02-11 13:10:13 +0800

    Update

--- src/routes/[unit]/+page.js
+++ src/routes/[unit]/+page.js
@@ -1,6 +1,6 @@
 export const load = ({ fetch, params }) => {
 	const fetchData = async (unit) => {
-		const res = await fetch(`https://apipltdktm.daengveloper.com/${unit}`);
+		const res = await fetch(`https://pltdktm.anpy.my.id/${unit}`);
 		const result = await res.json();
 		return result;
 	};
